2b57845807bf6efa5536ce0f7242b83391e64259
Xiangyu Sun <xiangyu.sun@glovoapp.com>
2022-06-14 22:29:32 +0200

    migrate to structured concurrency

--- Metronome/MainViewController.swift
+++ Metronome/MainViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import AVFoundation
 import MetromomeKit
 import os
+import Combine
 
 class MainViewController: UIViewController {
     var metronome: Metronome!
@@ -29,6 +30,9 @@ class MainViewController: UIViewController {
     @IBOutlet var swipeDownRestureRecongnizer: UISwipeGestureRecognizer!
     @IBOutlet var swipeUpGestureRecongnizer: UISwipeGestureRecognizer!
     @IBOutlet var swipeLeftGestureRecongnizer: UISwipeGestureRecognizer!
+    
+    private var cancellable = Set<AnyCancellable>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -40,11 +44,21 @@ class MainViewController: UIViewController {
         
         faceDrawer  = MetronomeFacePainter(faceConfiguration: faceConfig)
         metronome = Metronome(audioFormat: format)
-        metronome.delegate = self
-        faceDrawer.drawArchsWith(meter: metronome.meter) { (image) in
-           backgroudnImageView.image = image
+        
+        
+        Task { @MainActor in
+            backgroudnImageView.image = await faceDrawer.drawArchsWith(meter: metronome.meter)
         }
         
+        
+        metronome.tick
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+            print("end")
+        } receiveValue: { value in
+            self.updateArcWithTick(currentTick: value)
+        }
+        .store(in: &cancellable)
         
         NotificationCenter.default.addObserver(self, selector: #selector(handleMediaServicesWereReset), name: AVAudioSession.mediaServicesWereResetNotification, object: AVAudioSession.sharedInstance())
     }
@@ -55,108 +69,116 @@ class MainViewController: UIViewController {
     }
     @objc func handleMediaServicesWereReset()  {
         os_log("audio reset")
-        metronome.delegate = nil
-        metronome.reset()
-        
-        updateMeterLabel()
-        faceDrawer.drawArchsWith(meter: metronome.meter) { (image) in
-            backgroudnImageView.image = image
-        }
-        
-        metronome.delegate = self
-        
-        do {
-            try AVAudioSession.sharedInstance().setActive(true)
-        }catch {
-            os_log("%@", error.localizedDescription)
+ 
+        Task { @MainActor in
+            await metronome.reset()
+            
+            updateMeterLabel()
+            
+            backgroudnImageView.image = await faceDrawer.drawArchsWith(meter: metronome.meter)
+            do {
+                try AVAudioSession.sharedInstance().setActive(true)
+            }catch {
+                os_log("%@", error.localizedDescription)
+            }
         }
     }
   
 
     @IBAction func swipeDown(_ sender: Any) {
-        try? metronome.incrementDivisionIndex(by: -1)
-        updateMeterLabel()
+        Task {
+            try? await metronome.incrementDivisionIndex(by: -1)
+            updateMeterLabel()
+        }
     }
     @IBAction func swipeUp(_ sender: Any) {
-        try? metronome.incrementDivisionIndex(by: 1)
+        Task {
+            try? await metronome.incrementDivisionIndex(by: 1)
+        }
         updateMeterLabel()
     }
     @IBAction func swipeLeft(_ sender: Any) {
-        let wasRunning = metronome.isPlaying
-        
-        if wasRunning {
-            metronome.stop()
-        }
-        metronome.incrementMeter(by: 1)
-        faceDrawer.drawArchsWith(meter: metronome.meter) { (image) in
-            backgroudnImageView.image = image
-        }
-        updateMeterLabel()
-        
-        if wasRunning {
-            try? metronome.start()
+        Task {
+            let wasRunning = await metronome.isPlaying
+            
+            if wasRunning {
+                await metronome.stop()
+            }
+            await metronome.incrementMeter(by: 1)
+            
+            backgroudnImageView.image = await faceDrawer.drawArchsWith(meter: metronome.meter)
+            
+            updateMeterLabel()
+            
+            if wasRunning {
+                try? await metronome.start()
+            }
         }
     }
     @IBAction func swipeRight(_ sender: Any) {
-        let wasRunning = metronome.isPlaying
-        
-        if wasRunning {
-            metronome.stop()
-        }
-        metronome.incrementMeter(by: -1)
-        faceDrawer.drawArchsWith(meter: metronome.meter) { (image) in
-            backgroudnImageView.image = image
-        }
-        updateMeterLabel()
-        
-        if wasRunning {
-            try? metronome.start()
+        Task {
+            let wasRunning = await metronome.isPlaying
+            
+            if wasRunning {
+                await metronome.stop()
+            }
+            await metronome.incrementMeter(by: -1)
+            
+            backgroudnImageView.image = await faceDrawer.drawArchsWith(meter: metronome.meter)
+            
+            updateMeterLabel()
+            
+            if wasRunning {
+                try? await metronome.start()
+            }
         }
     }
     
     func updateArcWithTick(currentTick: Int) {
-        if metronome.isPlaying {
-            foregroundImageView.image = faceDrawer[currentTick]
-        } else {
-            foregroundImageView.image = nil
+        Task { @MainActor in
+            if await metronome.isPlaying {
+                foregroundImageView.image = await faceDrawer[currentTick]
+            } else {
+                foregroundImageView.image = nil
+            }
         }
     }
     @IBAction func tap(_ sender: Any) {
-       
-        if !metronome.isPlaying {
-            tempoTextField.resignFirstResponder()
-            try? metronome.start()
-        }else if metronome.isPlaying, !tempoTextField.resignFirstResponder() {
-            metronome.stop()
-            updateArcWithTick(currentTick: 0)
-            wasRunning = true
+        Task {
+            if await !metronome.isPlaying {
+                tempoTextField.resignFirstResponder()
+                try? await metronome.start()
+            }else if await metronome.isPlaying, !tempoTextField.resignFirstResponder() {
+                await metronome.stop()
+                updateArcWithTick(currentTick: 0)
+                wasRunning = true
+            }
         }
     }
 
     
     
     func updateTempoLabel() {
-        tempoTextField.text = "\(metronome.tempoBPM)"
+        Task {
+            tempoTextField.text = "\(await metronome.tempoBPM)"
+        }
     }
     
     
     func updateMeterLabel() {
-        meterLabel.text = "\(metronome.meter) / \(metronome.division)"
+        Task {
+            meterLabel.text = "\(await metronome.meter) / \(await metronome.division)"
+        }
+       
     }
 }
 
 extension MainViewController: UITextFieldDelegate {
     func textFieldDidEndEditing(_ textField: UITextField) {
-        guard let text = textField.text, let tempo = Int(text) else { return }
-        metronome.setTempo(to:tempo)
-        updateTempoLabel()
-    }
-}
-
-extension MainViewController: MetronomeDelegate {
-    func metronomeTicking(_ metronome: Metronome, currentTick: Int) {
-        DispatchQueue.main.async {
-            self.updateArcWithTick(currentTick: currentTick)
+        Task {
+            guard let text = textField.text, let tempo = Int(text) else { return }
+            await metronome.setTempo(to:tempo)
+            updateTempoLabel()
         }
     }
 }
--- MetronomeWatchOS Extension/InterfaceController.swift
+++ MetronomeWatchOS Extension/InterfaceController.swift
@@ -11,9 +11,10 @@ import Foundation
 import MetromomeKit
 import AVFoundation
 import os
+import Combine
 
 
-class InterfaceController: WKInterfaceController {
+final class InterfaceController: WKInterfaceController {
     @IBOutlet var backgroundArcsGroup: WKInterfaceGroup!
     @IBOutlet var foregroundArcsGroup: WKInterfaceGroup!
     @IBOutlet var tempoLabel: WKInterfaceLabel!
@@ -21,49 +22,64 @@ class InterfaceController: WKInterfaceController {
     
     var metronome: Metronome!
     var faceDrawer: MetronomeFacePainter!
-   
+    
     var accumulatedRotations: Double = 0
     var wasRunning = false
+    
+    private var cancellable = Set<AnyCancellable>()
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         guard let format = AVAudioFormat(standardFormatWithSampleRate: AVAudioSession.sharedInstance().sampleRate, channels: 1) else {
-            return 
+            return
         }
         let side = min(self.contentFrame.width, self.contentFrame.height)
         let frame = CGRect(x: 0, y: 0, width:  side, height: side)
         let faceConfig = FacePaintConfiguration(scale: WKInterfaceDevice.current().screenScale, contentFrame: frame)
         
         faceDrawer  = MetronomeFacePainter(faceConfiguration: faceConfig)
+        
         metronome = Metronome(audioFormat: format)
-        metronome.delegate = self
-        faceDrawer.drawArchsWith(meter: metronome.meter) { (image) in
-            backgroundArcsGroup.setBackgroundImage(image)
+
+        
+        Task { @MainActor in
+            await backgroundArcsGroup.setBackgroundImage( faceDrawer.drawArchsWith(meter: metronome.meter) )
         }
-      
         crownSequencer.delegate = self
-    
+        
+
+        metronome.tick
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+            print("end")
+        } receiveValue: { value in
+            self.updateArcWithTick(currentTick: value)
+        }
+        .store(in: &cancellable)
+
+        
         NotificationCenter.default.addObserver(self, selector: #selector(handleMediaServicesWereReset), name: AVAudioSession.mediaServicesWereResetNotification, object: AVAudioSession.sharedInstance())
     }
     
-
+    
     @objc func handleMediaServicesWereReset()  {
-        os_log("audio reset")
-        metronome.delegate = nil
-        metronome.reset()
-        
-        updateMeterLabel()
-        faceDrawer.drawArchsWith(meter: metronome.meter) { (image) in
-            backgroundArcsGroup.setBackgroundImage(image)
-        }
-        
-        metronome.delegate = self
-        
-        do {
-            try AVAudioSession.sharedInstance().setActive(true)
-        }catch {
-            os_log("%@", error.localizedDescription)
+        Task {
+            os_log("audio reset")
+            
+            await metronome.reset()
+            
+            updateMeterLabel()
+            
+            Task { @MainActor in
+                await backgroundArcsGroup.setBackgroundImage( faceDrawer.drawArchsWith(meter: metronome.meter) )
+            }
+            
+            do {
+                try AVAudioSession.sharedInstance().setActive(true)
+            }catch {
+                os_log("%@", error.localizedDescription)
+            }
         }
     }
     override func willActivate() {
@@ -77,115 +93,136 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
-        metronome.stop()
+        Task {
+            await metronome.stop()
+        }
     }
     
     func updateArcWithTick(currentTick: Int) {
-        if metronome.isPlaying {
-            foregroundArcsGroup.setBackgroundImage(faceDrawer[currentTick])
-        } else {
-            foregroundArcsGroup.setBackgroundImage(nil)
+        Task {
+            let isPlaying = await metronome.isPlaying
+            
+            if isPlaying {
+                
+                foregroundArcsGroup.setBackgroundImage( await faceDrawer[currentTick])
+                
+            } else {
+                foregroundArcsGroup.setBackgroundImage(nil)
+            }
         }
     }
-
+    
     func updateMeterLabel() {
-        meterLabel.setText("\(metronome.meter) / \(metronome.division)")
+        Task {
+            meterLabel.setText("\(await metronome.meter) / \(await metronome.division)")
+        }
     }
     
     func updateTempoLabel() {
-        tempoLabel.setText("\(metronome.tempoBPM) BPM")
+        Task {
+            tempoLabel.setText("\(await metronome.tempoBPM) BPM")
+        }
     }
     @IBAction func downSwipeRecongnized(_ sender: Any) {
-        try? metronome.incrementDivisionIndex(by: -1)
+        Task {
+            try? await metronome.incrementDivisionIndex(by: -1)
+        }
         updateMeterLabel()
     }
     @IBAction func upSwipeRecongnized(_ sender: Any) {
-        try? metronome.incrementDivisionIndex(by: 1)
+        Task {
+            try? await metronome.incrementDivisionIndex(by: 1)
+        }
         updateMeterLabel()
     }
     @IBAction func leftSwipeRecongnized(_ sender: Any) {
-        let wasRunning = metronome.isPlaying
-        
-        if wasRunning {
-            metronome.stop()
-        }
-        metronome.incrementMeter(by: 1)
-        faceDrawer.drawArchsWith(meter: metronome.meter) { (image) in
-            backgroundArcsGroup.setBackgroundImage(image)
-        }
-        updateMeterLabel()
-        
-        if wasRunning {
-            try? metronome.start()
+        Task {
+            let wasRunning =  await metronome.isPlaying
+            
+            if wasRunning {
+                await metronome.stop()
+            }
+            await metronome.incrementMeter(by: 1)
+            
+            Task { @MainActor in
+                await backgroundArcsGroup.setBackgroundImage( faceDrawer.drawArchsWith(meter: metronome.meter) )
+            }
+            
+            updateMeterLabel()
+            
+            if wasRunning {
+                try? await metronome.start()
+            }
         }
     }
     @IBAction func rightSwipeRecongnized(_ sender: Any) {
-        let wasRunning = metronome.isPlaying
-        
-        if wasRunning {
-            metronome.stop()
-        }
-        metronome.incrementMeter(by: -1)
-        faceDrawer.drawArchsWith(meter: metronome.meter) { (image) in
-            backgroundArcsGroup.setBackgroundImage(image)
-        }
-        updateMeterLabel()
-        
-        if wasRunning {
-            try? metronome.start()
+        Task {
+            let wasRunning = await metronome.isPlaying
+            
+            if wasRunning {
+                await metronome.stop()
+            }
+            await metronome.incrementMeter(by: -1)
+            
+            Task { @MainActor in
+                await backgroundArcsGroup.setBackgroundImage( faceDrawer.drawArchsWith(meter: metronome.meter) )
+            }
+            
+            updateMeterLabel()
+            
+            if wasRunning {
+                try? await metronome.start()
+            }
         }
     }
     @IBAction func tapRecongnized(_ sender: Any) {
-        if metronome.isPlaying {
-            metronome.stop()
-            updateArcWithTick(currentTick: 0)
-            wasRunning = true
-        }else {
-            try? metronome.start()
+        Task {
+            if await metronome.isPlaying {
+                await metronome.stop()
+                updateArcWithTick(currentTick: 0)
+                wasRunning = true
+            }else {
+                try? await metronome.start()
+            }
         }
     }
-
     
     
     
-}
-
-
-extension InterfaceController: MetronomeDelegate {
-    func metronomeTicking(_ metronome: Metronome, currentTick: Int) {
-        DispatchQueue.main.async {
-            self.updateArcWithTick(currentTick: currentTick)
-        }
-    }
+    
 }
 
 extension InterfaceController: WKCrownDelegate {
     func crownDidRotate(_ crownSequencer: WKCrownSequencer?, rotationalDelta: Double) {
-        if metronome.isPlaying {
-            metronome.stop()
-            updateArcWithTick(currentTick: 0)
-            wasRunning = true
-        }
-        var value = 0
-        accumulatedRotations += rotationalDelta
-        
-        if accumulatedRotations >= 0.15 {
-            value = 1;
-            accumulatedRotations = 0;
-        } else if accumulatedRotations <= -0.15 {
-            value = -1;
-            accumulatedRotations = 0;
-        }
-        if  value != 0 {
-            metronome.incrementTempo(by: value)
-            updateTempoLabel()
+        Task {
+            if await metronome.isPlaying {
+                await metronome.stop()
+                updateArcWithTick(currentTick: 0)
+                wasRunning = true
+            }
+            var value = 0
+            accumulatedRotations += rotationalDelta
+            
+            if accumulatedRotations >= 0.15 {
+                value = 1;
+                accumulatedRotations = 0;
+            } else if accumulatedRotations <= -0.15 {
+                value = -1;
+                accumulatedRotations = 0;
+            }
+            if  value != 0 {
+                await metronome.incrementTempo(by: value)
+                updateTempoLabel()
+            }
         }
     }
     
     func crownDidBecomeIdle(_ crownSequencer: WKCrownSequencer?) {
-        if wasRunning {
-            try? metronome.start()
-            wasRunning = false
+        Task {
+            if wasRunning {
+                try? await metronome.start()
+                wasRunning = false
+            }
         }
     }
 }
